ded53d1fb4c9c90dde56e1ec55382baaa45990f4
Francesca <158456703+FrancescaCapra@users.noreply.github.com>
2025-01-04 16:42:58 +0100

    Update Main.tsx
    whoami

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,12 +16,12 @@ function Main() {
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Francesca Capra</h1>
+          <p>Cybersecurity Engineering</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/FrancescaCapra" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/francescacapra/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
