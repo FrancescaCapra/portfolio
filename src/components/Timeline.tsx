2b831f5fd7ec10f7f0203dad7b13b50947835af1
Francesca <158456703+FrancescaCapra@users.noreply.github.com>
2025-01-04 18:28:17 +0100

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss';
 
@@ -20,11 +20,12 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">EIT Digital Master’s in Cybersecurity</h3>
-            <h4 className="vertical-timeline-element-subtitle">University of Turku, Finland & University of Trento, Italy</h4>
+            <h3 className="vertical-timeline-element-title">EIT Digital Master’s – Double Degree in Cybersecurity</h3>
+            <h4 className="vertical-timeline-element-subtitle">University of Trento & University of Turku</h4>
             <p>
-              Focus: Network Security, Security Testing, Cybersecurity Risk Assessment, Distributed Systems
+              Acquisition of skills in penetration testing, network security, and basic ethical hacking.
             </p>
+            <p><strong>Relevant courses:</strong> Software Security Testing, Distributed Systems, Network Security, Software Design and Architecture, Firewall and IPS, Cybersecurity Risk Assessment, Privacy and IPR.</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
@@ -32,11 +33,12 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Bachelor’s in Computer Engineering</h3>
+            <h3 className="vertical-timeline-element-title">Bachelor’s Degree in Computer Engineering</h3>
             <h4 className="vertical-timeline-element-subtitle">Politecnico di Torino, Italy</h4>
             <p>
-              Focus: Algorithms, Data Structure, Operating Systems, Database Systems, Object-Oriented Programming
+              Focused on computer science and engineering with hands-on experience in various technical disciplines.
             </p>
+            <p><strong>Relevant courses:</strong> Algorithms and Programming Techniques, Computer Architecture, Introduction to Databases, Object-Oriented Programming, Computer Networks, Operating Systems.</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -47,8 +49,9 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Graphic Designer</h3>
             <h4 className="vertical-timeline-element-subtitle">Kernel Sistemi, Turin, Italy</h4>
             <p>
-              Focus: Advertising Campaigns, LinkedIn Engagement, Visual Content Creation
+              Increased LinkedIn followers by 25% by managing daily corporate updates. Designed impactful advertising campaigns that led to a 45% increase in event attendance.
             </p>
+            <p>Measured key project indicators like engagement rate, number of followers, and click-through rate (CTR).</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -59,13 +62,6 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Waitress</h3>
             <h4 className="vertical-timeline-element-subtitle">Signorvino, Turin, Italy</h4>
             <p>
-              Improved customer service efficiency and product knowledge, leading to increased wine sales.
+              Memorized and accurately described wine varieties to customers, boosting wine sales by 10% over 6 months. 
             </p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
-      </div>
-    </div>
-  );
-}
-
-export default Timeline;
+            <p>Handled an average of 25-30 tables per 
