6216b660dc6f73c3f8c8a715782a8a6943ace2c0
Francesca <158456703+FrancescaCapra@users.noreply.github.com>
2025-01-04 18:18:34 +0100

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,47 +16,35 @@ function Timeline() {
             className="vertical-timeline-element--education"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2023 - Present"
+            date="09/2023 - Present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">EIT Digital Master’s in Cybersecurity</h3>
             <h4 className="vertical-timeline-element-subtitle">University of Turku, Finland & University of Trento, Italy</h4>
             <p>
-              Focus: Network Security, Cybersecurity Risk Assessment, Distributed Systems
+              Focus: Network Security, Security Testing, Cybersecurity Risk Assessment, Distributed Systems
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="2019 - 2023"
+            date="09/2019 - 08/2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Bachelor’s in Computer Engineering</h3>
             <h4 className="vertical-timeline-element-subtitle">Politecnico di Torino, Italy</h4>
             <p>
-              Focus: Algorithms, Operating Systems, Database Systems, Object-Oriented Programming
+              Focus: Algorithms, Data Structure, Operating Systems, Database Systems, Object-Oriented Programming
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2023 - Present"
+            date="03/2020 - 09/2020"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Cybersecurity Analyst</h3>
-            <h4 className="vertical-timeline-element-subtitle">Turku, Finland</h4>
-            <p>
-              Projects: Penetration Testing, Cybersecurity Risk Assessment, BGP Protocol Simulator
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2020"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Graphic Designer Intern</h3>
+            <h3 className="vertical-timeline-element-title">Graphic Designer</h3>
             <h4 className="vertical-timeline-element-subtitle">Kernel Sistemi, Turin, Italy</h4>
             <p>
               Focus: Advertising Campaigns, LinkedIn Engagement, Visual Content Creation
@@ -64,7 +52,7 @@ function Timeline() {
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2022 - 2023"
+            date="12/2022 - 07/2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
